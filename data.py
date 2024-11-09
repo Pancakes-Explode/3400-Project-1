0b19353875fc94be805f58947b0b790737e2a479
Pancakes-Explode2 <Rhett.hill@selu.edu>
2024-11-09 11:10:19 -0600

    Display Current waether

--- data.py
+++ data.py
@@ -1,7 +1,42 @@
-import openmeteo_requests
-
-import requests_cache
+from ast import Param
+import requests
 import pandas as pd
-from retry_requests import retry
+
+
+class Data:
+
+    params = None
+    info = None
+    
+    def __init__(self,lat, long):
+        self.params =  {
+    "latitude": lat,
+    "longitude": long,
+    "current_weather": "true",
+    "temperature_unit": "fahrenheit"
+}
+        
+    #
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    def getCurrentWeather(self):
+        # returns the dictionary for current weather
+        self.response = requests.get(self.url, params=self.params)
+        data = self.response.json()
+        return data.get('current_weather', None)
+    #
+    def display_Weather(self):
+        #Prints current weather conditions for given Location
+        weather_data = self.getCurrentWeather()
+        
+        if weather_data:
+            print(f"{weather_data}")
+        else:
+            print("Failed to retrieve weather data.")
+    #
+
+
+
+
 
 
