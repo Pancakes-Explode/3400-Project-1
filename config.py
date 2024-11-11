66e8ffa0411b6e09e2775a9c4fdd3608dbf7bd31
Pancakes-Explode2 <Rhett.hill@selu.edu>
2024-11-11 14:23:18 -0600

    Structure

--- config.py
+++ config.py
@@ -7,12 +7,16 @@ class Data:
 
     params = None
     info = None
+    lat = None
+    long = None
     
     def __init__(self,lat, long):
+        self.lat = lat
+        self.long = long
         self.params =  {
     "latitude": lat,
     "longitude": long,
-    "current_weather": "true",
+    "current": ["temperature_2m", "relative_humidity_2m", "is_day", "precipitation", "rain", "weather_code", "wind_speed_10m"],
     "temperature_unit": "fahrenheit"
 }
         
@@ -23,17 +27,16 @@ class Data:
         # returns the dictionary for current weather
         self.response = requests.get(self.url, params=self.params)
         data = self.response.json()
-        return data.get('current_weather', None)
+        return pd.DataFrame(data['current'], index=[1])
     #
     def display_Weather(self):
         #Prints current weather conditions for given Location
         weather_data = self.getCurrentWeather()
+        weather_data.to_csv(path_or_buf= f"output/{self.lat},{self.long}")
+
         
-        if weather_data:
-            print(f"{weather_data}")
-        else:
-            print("Failed to retrieve weather data.")
-    #
+        print(f"{weather_data}")
+         #
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import os;
-from data import Data
+from config import Data
 
 
 if __name__ == "__main__":
